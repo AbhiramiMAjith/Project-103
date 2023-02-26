d5d3098b5a73d0810dc66af78be18d3d0a2bfa28
AbhiramiMAjith <98221621+AbhiramiMAjith@users.noreply.github.com>
2023-02-26 20:06:10 +0530

    Add files via upload

---
+++ Project 103/event_tracker.py
@@ -0,0 +1,34 @@
+import sys
+import time
+import random
+
+import os
+import shutil
+
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+
+from_dir = "C:/Users/Abhirami M Ajith/Downloads"
+
+class FileEventHandler(FileSystemEventHandler):
+    def on_created(self, event):
+        print(f"The file {event.src_path} has been created")
+    def on_deleted(self, event):
+        print(f"The file {event.src_path} has been deleted")
+    def on_modified(self, event):
+        print(f"The file {event.src_path} has been modiefied")
+    def on_moved(self, event):
+        print(f"The file {event.src_path} has been moved")
+        
+event_handler = FileEventHandler()
+observer = Observer()
+observer.schedule(event_handler, from_dir, recursive=True)
+observer.start()
+
+try:
+    while True:
+        time.sleep(3)
+        print("running...")
+except KeyboardInterrupt:
+    print("stop")
+    observer.stop()
